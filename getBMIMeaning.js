b9dc504ba3137fec6b7ed304be36795caadbb7d7
Tanyathip Aoongern <Tanyathip.aoongern@mail.kmutt.ac.th>
2022-09-05 19:47:07 +0700

    Homework bmi-1

--- getBMIMeaning.js
+++ getBMIMeaning.js
@@ -1,9 +1,26 @@
 const { template } = require('@babel/core')
 
 function calculateBMI(weight, height) {
-  //code here
+  BMI = weight/(height*height)
+  return BMI
+  
 }
 function getBMIMeaning(weight, height) {
-  //code here
+    calculateBMI(weight, height)
+    if (BMI<18.5) {
+        console.log("Underweight")
+        return "Underweight"
+    }
+    if (BMI<=24.9) {
+        if (BMI>=18.5) {
+            console.log("Normal weight")
+            return "Normal weight"
+        }
+    }
+    if (BMI>25.0) {
+        console.log("Overweight")
+        return "Overweight"
+    }
+
 }
 module.exports = getBMIMeaning
